996177647b88e7de932b2dd9912d93d8c1c198c2
Unai Molina <unai.molina.gordillo@gmail.com>
2013-09-29 21:18:42 +0100

    formatted to html5 format

--- src/main/scala/sukil/snippet/Welcome.scala
+++ src/main/scala/sukil/snippet/Welcome.scala
@@ -2,7 +2,13 @@ package sukil
 package snippet
 
 import net.liftweb.util.Props
+import net.liftweb.util.Helpers._
+import scala.xml.NodeSeq
 
 class Welcome{
-  def hello = <span>Welcome to this site at {new _root_.java.util.Date}. Mode is {Props.mode}</span>
+  def hello = <span>Welcome to my new site at {new _root_.java.util.Date}. Mode is {Props.mode}</span>
+
+  def status(xhtml: NodeSeq) = {
+    bind("mystatus",xhtml, "name" -> "excited")
+  }
 }
